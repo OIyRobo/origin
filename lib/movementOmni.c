adb3e7b5ee13dfffb3884d524f088acf3c9ab286
David 'The Big D' Garber <daggad11@gmail.com>
2013-11-20 09:04:52 -0800

    fixed autonomous mapping

--- lib/movementOmni.c
+++ lib/movementOmni.c
@@ -18,10 +18,10 @@ void move(int dir, int power, int time) {
 		motor[motorBR] = power;
 	}
 	else if (dir == BACKWARDS) {
-		motor[motrFL] = -power;
-		motor[motrFR] = -power;
-		motor[motrBL] = -power;
-		motor[motrBR] = -power;
+		motor[motorFL] = -power;
+		motor[motorFR] = -power;
+		motor[motorBL] = -power;
+		motor[motorBR] = -power;
 	}
 	wait1Msec(time);
 	motor[motorFL] = 0;
