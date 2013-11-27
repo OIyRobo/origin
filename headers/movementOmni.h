7eb84c657a6649152e9f0285b9fe97192f1f56fb
David 'The Big D' Garber <daggad11@gmail.com>
2013-11-27 09:09:01 -0800

    fixed omni header

--- headers/movementOmni.h
+++ headers/movementOmni.h
@@ -3,10 +3,10 @@
 
 const int NE = 0;
 const int NW = 1;
-void move(float angle, int power);
+void move(float theta, int power);
 void turn(int degrees);
 void brake();
-void maximizePower(float *powers); //Maximizes the powers on the motors for translating at maximum speed. The powers array is passed by reference.
+void maximizePower(float *powers, int power); //Maximizes the powers on the motors for translating at maximum speed. The powers array is passed by reference.
 
 #include "..\lib\movementOmni.c"
 #endif
