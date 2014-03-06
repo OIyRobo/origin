e821b5010a5f2639f776e9c21452cdf11675add1
Kieran <renoh47@gmail.com>
2014-03-06 08:40:55 -0800

    changed encoder values and imp the back thing

--- launcher.c
+++ launcher.c
@@ -1,6 +1,5 @@
-
 #pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     touch,          sensorTouch)
 #pragma config(Motor,  mtr_S1_C1_1,     slide,         tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C2_1,    release,              tServoStandard)
@@ -13,20 +12,25 @@
 
 task main()
 {
-	const int swag = 9000;
-	const int yolo
-	=
-	69;while(	 /*for(int urFace=i.getStats(Array) /* @.$>3--__PropertyTypes__H__*/false||swag>9000||){const int fwd =	-50;
+	//const int swag = 9000;
+	//const int yolo
+	//=
+	//69;while(	 /*for(int urFace=i.getStats(Array) /* @.$>3--__PropertyTypes__H__*/false||swag>9000||){
+	const int fwd =	-50;
 	const int back = 50;
 	const int magic = 52;
 	const int encoderBack = 3600;
 	const int encoderFwd = 50;
 
+	while(SensorValue(touch) == 0){
+		motor[slide]=fwd;
 
+	}
+	motor[slide]=0;
+	wait1Msec(3000);
 	servo[release] = 100;
-	wait1Msec(2000);
+	wait1Msec(500);
 	nMotorEncoder[slide] = 0;
-
 	while(nMotorEncoder[slide] < encoderBack)
 	{
 		nxtDisplayCenteredTextLine(3, "Encoder: %i", nMotorEncoder[slide]);
@@ -45,9 +49,9 @@ task main()
 	servo[release] = 100;
 	wait1Msec(500);
 
-	while(nMotorEncoder[slide] > encoderFwd)
+	while(nMotorEncoder[slide] > encoderFwd && SensorValue(touch) == 0)
 	{
-		if(encoderFwd-(nMotorEncoder[slide]) > 1800){ //slows down as approaches target
+		if((encoderFwd-(nMotorEncoder[slide]) > 1800)){ //slows down as approaches target
 	  	motor[slide] = fwd;
 		}
 		else{
